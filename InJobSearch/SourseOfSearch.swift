bf30171c80a0a745fc12cd0baa37ead33de9c614
Dmitry Roytman <d@geans.ru>
2015-09-26 14:17:57 +0500

    Adding case .UpWork to enum SourseOfSearch

--- InJobSearch/SourseOfSearch.swift
+++ InJobSearch/SourseOfSearch.swift
@@ -9,14 +9,31 @@
 import Foundation
 
 enum SourseOfSearch {
-    case UpWork
+    case UpWork(categories: [SearchCategory])
     
 //    var entityValue
-    
-    func getBaseURL() -> NSURL! {
+    func basePath() -> String {
         switch self {
-            
-        default: break
+        case .UpWork(_): return "https://www.upwork.com/jobs/rss?"
+        }
+    }
+    
+    func path() -> String {
+        var base = self.basePath()
+        switch self {
+        case .UpWork(let categories):
+            for (index, category) in enumerate(categories) { base = base + "cn\(index+1)=" + category.pathComponent + "&" }
+            base = base + "q=&from=find-work"
+            Log.m(base)
+            return base
+        default: Log.d("SourseOfSearch.\(__FUNCTION__): unknown sourse"); return ""
+        }
+    }
+    
+    func url() -> NSURL! {
+        switch self {
+        case .UpWork(_): return NSURL(string: self.path())
+        default: Log.d("SourseOfSearch.\(__FUNCTION__): unknown sourse")
         }
         return nil
     }
