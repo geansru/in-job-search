2df25d4007c64b8b841d834f9b2656b7d840387f
Dmitry Roytman <d@geans.ru>
2015-09-26 16:13:45 +0500

    Finish with Parser class. Add JobUpWork class - subclass of Job

--- InJobSearch/Parser.swift
+++ InJobSearch/Parser.swift
@@ -14,11 +14,60 @@ enum DataType {
     case Image
 }
 
+protocol ParserDelegate {
+    func parseWillStart(parse: Parser)
+    func parseDidStart(parse: Parser)
+//    func parseDidReceiveError(parse: Parser, error: NSError)
+    func parseDidReceiveError(parse: Parser, errorString: String)
+    func parseWillFinish(parse: Parser)
+    func parseDidFinish(parse: Parser, result: [AnyObject]!)
+}
+
 protocol Parseable {
     var data: NSData! { get set }
     var dataType:DataType { get set }
+    var sourse: SourseOfSearch { get set }
 }
 
 class Parser {
+    var object: Parseable
+    var delegate: ParserDelegate!
+    init(object: Parseable) {
+        self.object = object
+    }
     
+    convenience init(object: Parseable, delegate: ParserDelegate) {
+        self.init(object: object)
+        self.delegate = delegate
+        parse()
+    }
+    
+    func parse() {
+        delegate?.parseWillStart(self)
+        switch object.sourse {
+        case .UpWork(_): parseUpwork()
+        }
+    }
+    
+    func parseUpwork() {
+        var result = [Job]()
+        delegate?.parseDidStart(self)
+        Log.m("TODO in \(__FUNCTION__)")
+        switch object.dataType {
+        case .XML:
+            if let data = object.data {
+                if let xmlString = NSString(data: data, encoding: NSUTF8StringEncoding){
+                    let job = JobUpWork(xmlString: xmlString)
+                    result.append(job)
+                } else {
+                    delegate.parseDidReceiveError(self, errorString: "Cann't process NSdata to NSString in \(__FUNCTION__)")
+                }
+            } else {
+                delegate.parseDidReceiveError(self, errorString: "NSdata object is nil in \(__FUNCTION__)")
+            }
+            delegate?.parseWillFinish(self)
+        default: delegate?.parseDidReceiveError(self, errorString: "Unusable data type: \(object.sourse)")
+        }
+        delegate?.parseDidFinish(self, result: result)
+    }
 }
--- InJobSearch/SearchEntity.swift
+++ InJobSearch/SearchEntity.swift
@@ -9,5 +9,25 @@
 import Foundation
 
 class SearchEntity: Downloadable, Parseable {
-    var categories: [SearchCategory]
+    // MARK: - Downloadable deleagate
+    var url: NSURL!
+    var sourse: SourseOfSearch
+    var dataTask: NSURLSessionDataTask!
+    
+    // MARK: - Properties
+    var categories: [SearchCategory] = []
+    
+    // MARK: - Parseable deleagate
+    var data: NSData!
+    var dataType:DataType = .XML
+    
+    init(sourse: SourseOfSearch) {
+        self.sourse = sourse
+        switch sourse {
+        case .UpWork(let categories):
+            self.categories = categories
+            self.url = sourse.url()
+            Log.m(self.url?.description ?? "\(__FUNCTION__): url is empty")
+        }
+    }
 }
