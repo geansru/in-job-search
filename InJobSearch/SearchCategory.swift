1fb5a7ed15a11f29d7624f3427429628f718b7f2
Dmitry Roytman <d@geans.ru>
2015-09-26 13:47:29 +0500

    Add SearchCategory model object

---
+++ InJobSearch/SearchCategory.swift
@@ -0,0 +1,15 @@
+//
+//  SearchCategory.swift
+//  InJobSearch
+//
+//  Created by Dmitriy Roytman on 26.09.15.
+//  Copyright (c) 2015 Dmitriy Roytman. All rights reserved.
+//
+
+import Foundation
+
+class SearchCategory {
+    var title = ""
+    var description = ""
+    var pathComponent = ""
+}
