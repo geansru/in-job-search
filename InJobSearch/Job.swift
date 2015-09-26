54b46d2e6d44f5382953d13c0aba0a0bee339b8e
Dmitry Roytman <d@geans.ru>
2015-09-26 13:45:33 +0500

    Add Job model class

---
+++ InJobSearch/Job.swift
@@ -0,0 +1,19 @@
+//
+//  Job.swift
+//  InJobSearch
+//
+//  Created by Dmitriy Roytman on 26.09.15.
+//  Copyright (c) 2015 Dmitriy Roytman. All rights reserved.
+//
+
+import Foundation
+
+class Job {
+    var title = ""
+    var link = ""
+    var description = ""
+    var content = ""
+    var pubDate = ""
+    var favourite = true
+    var proposals = 0
+}
